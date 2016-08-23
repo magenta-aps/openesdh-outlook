4b535b610df5692994e412fa41ba7b78aca247aa
beemen <beshara_beemen@hotmail.com>
2016-08-23 10:09:45 +0200

    Code refactoring for ribbon class

--- OpenEsdh.2013.Word/OpenEsdh/_2013/Word/ThisRibbonCollection.cs
+++ OpenEsdh.2013.Word/OpenEsdh/_2013/Word/ThisRibbonCollection.cs
@@ -5,13 +5,8 @@
     using System.CodeDom.Compiler;
     using System.Diagnostics;
 
-    [GeneratedCode("Microsoft.VisualStudio.Tools.Office.ProgrammingModel.dll", "12.0.0.0"), DebuggerNonUserCode]
-    internal sealed class ThisRibbonCollection : RibbonCollectionBase
+    internal sealed partial class ThisRibbonCollection : RibbonCollectionBase
     {
-        internal ThisRibbonCollection(Microsoft.Office.Tools.Ribbon.RibbonFactory factory) : base((Microsoft.Office.Tools.Ribbon.RibbonFactory) factory)
-        {
-        }
-
         internal OpenEsdh._2013.Word.OpenESDHRibbon OpenESDHRibbon
         {
             get
